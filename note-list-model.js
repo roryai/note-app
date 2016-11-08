383dd0fb64eaa45eb1b5cc2f8c8035290e999da6
Rory Aitchison <rory.aitchison@gmail.com>
2016-11-08 15:01:30 +0000

    day 2

--- note-list-model.js
+++ note-list-model.js
@@ -1,3 +1,4 @@
+(function(exports){
 function NoteList(){
   this.noteArray = [];
 };
@@ -6,6 +7,15 @@ NoteList.prototype.notePrinter = function () {
     return this.noteArray;
 };
 
-NoteList.prototype.storeNote = function(noteObj) {
-  this.noteArray.push(new Note(noteObj));
+NoteList.prototype.storeNote = function(text) {
+  this.noteArray.push(new Note(text));
 };
+
+exports.NoteList = NoteList;
+// exports.notePrinter = this.notePrinter;
+// exports.storeNote = this.storeNote;
+
+})(this);
+
+notes = <li><div> adsfasdfdsaf </li></div>
+return "<ul>" + notes + "</ul>"
--- noteSpec.js
+++ noteSpec.js
@@ -1,36 +1,31 @@
 function storesTextOnCreation(){
   var note = new Note("text")
-
   if (note.textReturn() != "text") {
-    throw "False"
+    console.log("false")
   } else {
     console.log("true")
-    }
-  };
+  }
+};
 
 function noteListStoresNote() {
   var noteList = new NoteList();
-  noteList.notePrinter();
-  noteList.storeNote("testtext")
-
-  if (!noteList.noteArray[0] instanceof Note) {
-    throw "False"
+  noteList.storeNote("test text")
+  if (noteList.noteArray[0].text != "test text") {
+    console.log("false")
   } else {
     console.log("true")
-    }
+  }
 };
 
 function noteListPrintsNotes() {
   var noteList = new NoteList();
   noteList.storeNote("harry potter");
-  console.log(noteList.notePrinter())
   if (noteList.notePrinter() == noteList.noteArray) {
     console.log("yer a wizard, 'arry'")
   } else {
-    throw "False!"
+    console.log("false")
   }
 };
-
 
 storesTextOnCreation();
 noteListStoresNote();
