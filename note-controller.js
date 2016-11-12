0af574957160d055170c952c04a775548d064325
Rory Aitchison <rory.aitchison@gmail.com>
2016-11-12 13:04:02 +0000

    adds updated controller

--- note-controller.js
+++ note-controller.js
@@ -18,19 +18,3 @@
   exports.NoteController = NoteController;
 
 })(this);
-
-
-
-// NoteController.prototype.singleNoteDivInit = function () {
-//   this.noteListView.divCreator("single");
-//   this.noteListView.divPopulator(this.singleNoteView.htmlNote(), "single");
-// };
-
-// this.noteListView.divPopulator(noteController.testHtmlList(), "app");
-
-// NoteController.prototype.testHtmlList = function () {
-//   this.newNote("pray, eat monkey, pray, jump, run");
-//   this.newNote("pray, eat, eat");
-//   var joiner = this.noteListView.htmlJoiner();
-//   return joiner;
-// };
