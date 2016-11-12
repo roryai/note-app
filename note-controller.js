2770ab9efdc16b48a9156a4f5c2a770bb9bebfb4
Rory Aitchison <rory.aitchison@gmail.com>
2016-11-12 22:17:52 +0000

    it's working, it's working

--- note-controller.js
+++ note-controller.js
@@ -3,12 +3,11 @@
   function NoteController(){
     this.noteList = new NoteList();
     this.noteListView = new NoteListView(this.noteList);
-    this.singleNoteView = new SingleNoteView(new Note);
+    // this.singleNoteView = new SingleNoteView(new Note);
   }
 
   NoteController.prototype.divInit = function () {
     this.noteListView.divCreator("app");
-    console.log(44)
     this.noteListView.divPopulator(this.noteListView.createsListOfLinks(), "app");
   };
 
@@ -16,39 +15,32 @@
       this.noteList.storeNote(new Note(text));
   };
 
-  NoteController.prototype.eventListener = function () {
-    // console.log(99)
-    window.addEventListener("hashchange", this.showClickedNoteOnPage());
-  };
-
-  NoteController.prototype.showClickedNoteOnPage = function () {
-    //   console.log(this.getNoteIdFromURL(window.location))
-    // console.log(this.getNoteTextById(this.getNoteIdFromURL(window.location)))
-    //
-    // console.log(this.exportNoteTextToDiv(this.getNoteTextById(this.getNoteIdFromURL(window.location))))
-    this.exportNoteTextToDiv(this.getNoteTextById(this.getNoteIdFromURL(window.location)));
-  };
-
-  NoteController.prototype.getNoteIdFromURL = function (location) {
-    return location.hash.split("#")[1];
-  };
-
-  NoteController.prototype.getNoteTextById = function (id) {
-    for (i = 0; i < this.noteList.noteArray.length; i++) {
-      if (this.noteList.noteArray[i].id == [id]) {
-        return this.noteList.noteArray[i].text
-      }
-    }
-  };
-
-  NoteController.prototype.exportNoteTextToDiv = function (text) {
-    // console.log(document.getElementById("test"))
-    console.log(text)
-    console.log(document)
-    console.log(3)
-    console.log(document.getElementById("test"))
-    document.getElementById("test").innerHTML = text;
-  };
+  // NoteController.prototype.eventListener = function () {
+  //   window.addEventListener("hashchange", this.showClickedNoteOnPage);
+  // };
+  //
+  // NoteController.prototype.showClickedNoteOnPage = function () {
+  //
+  //   var aa = this.getNoteTextById(this.getNoteIdFromURL(window.location));
+  //   console.log(aa);
+  //   this.exportNoteTextToAppDiv(aa);
+  // };
+  //
+  // NoteController.prototype.getNoteIdFromURL = function (location) {
+  //   return location.hash.split("#")[1];
+  // };
+  //
+  // NoteController.prototype.getNoteTextById = function (id) {
+  //   for (i = 0; i < this.noteList.noteArray.length; i++) {
+  //     if (this.noteList.noteArray[i].id == [id]) {
+  //       return this.noteList.noteArray[i].text
+  //     }
+  //   }
+  // };
+  //
+  // NoteController.prototype.exportNoteTextToAppDiv = function (text) {
+  //   document.getElementById("test").innerHTML = text;
+  // };
 
   exports.NoteController = NoteController;
 
