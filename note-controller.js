7d87a1a21c7a1e1ad62d5e71eed0940380f782b8
Rory Aitchison <rory.aitchison@gmail.com>
2016-11-12 22:21:03 +0000

    bizarre...

--- note-controller.js
+++ note-controller.js
@@ -15,32 +15,32 @@
       this.noteList.storeNote(new Note(text));
   };
 
-  // NoteController.prototype.eventListener = function () {
-  //   window.addEventListener("hashchange", this.showClickedNoteOnPage);
-  // };
-  //
-  // NoteController.prototype.showClickedNoteOnPage = function () {
-  //
-  //   var aa = this.getNoteTextById(this.getNoteIdFromURL(window.location));
-  //   console.log(aa);
-  //   this.exportNoteTextToAppDiv(aa);
-  // };
-  //
-  // NoteController.prototype.getNoteIdFromURL = function (location) {
-  //   return location.hash.split("#")[1];
-  // };
-  //
-  // NoteController.prototype.getNoteTextById = function (id) {
-  //   for (i = 0; i < this.noteList.noteArray.length; i++) {
-  //     if (this.noteList.noteArray[i].id == [id]) {
-  //       return this.noteList.noteArray[i].text
-  //     }
-  //   }
-  // };
-  //
-  // NoteController.prototype.exportNoteTextToAppDiv = function (text) {
-  //   document.getElementById("test").innerHTML = text;
-  // };
+  NoteController.prototype.eventListener = function () {
+    window.addEventListener("hashchange", this.showClickedNoteOnPage);
+  };
+
+  NoteController.prototype.showClickedNoteOnPage = function () {
+
+    var aa = this.getNoteTextById(this.getNoteIdFromURL(window.location));
+    console.log(aa);
+    this.exportNoteTextToAppDiv(aa);
+  };
+
+  NoteController.prototype.getNoteIdFromURL = function (location) {
+    return location.hash.split("#")[1];
+  };
+
+  NoteController.prototype.getNoteTextById = function (id) {
+    for (i = 0; i < this.noteList.noteArray.length; i++) {
+      if (this.noteList.noteArray[i].id == [id]) {
+        return this.noteList.noteArray[i].text
+      }
+    }
+  };
+
+  NoteController.prototype.exportNoteTextToAppDiv = function (text) {
+    document.getElementById("test").innerHTML = text;
+  };
 
   exports.NoteController = NoteController;
 
